1fb4e7ea62adb1fc2688ed9132c06a1988db1181
Mitalipatel22 <vamjamitali2824@gmail.com>
2022-07-02 18:31:30 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
+import React from "react";
+import "../styles/App.css";
+//import Title from "./Title";
+//import Description from "./Description";
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name">MovieList</h1>
+      <div data-ns-test="project-description">
+        This app gives you the features to like the movie, add to watchlist, or
+        remove from movielist.
+      </div>
+    </>
+  );
+};
 
 export default App;
