0142ea5e5598b1d7feacd0162c492658d12876a2
Mitalipatel22 <vamjamitali2824@gmail.com>
2022-07-02 19:04:24 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
-import React from "react";
-import "../styles/App.css";
-//import Title from "./Title";
-//import Description from "./Description";
+import { render } from "enzyme";
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+
+
+function Project(){
+  return(
+    <div>
+    <h1 data-ns-test="project-name">Clone of an E-commerce site</h1>
+    <p data-ns-test="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro delectus ex quaerat esse maxime qui, obcaecati a quam et quis sapiente rem impedit inventore voluptas omnis quod eum repellendus cupiditate.</p>
+    </div>
+  );
+}
 const App = () => {
   return (
-    <>
-      <h1 data-ns-test="project-name">MovieList</h1>
-      <div data-ns-test="project-description">
-        This app gives you the features to like the movie, add to watchlist, or
-        remove from movielist.
-      </div>
-    </>
-  );
-};
+    <div id="main">
+          <Project />
+    </div>
+  )
+}
+
 
 export default App;
